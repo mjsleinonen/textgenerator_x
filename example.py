b707062f1e5e91766a42811d4d3abdb794b5a026
mjleinon <37039101+mjleinon@users.noreply.github.com>
2018-03-04 13:20:52 +0200

    example of generated lines

---
+++ example.py
@@ -0,0 +1,23 @@
+
+def example(filename,arg1,arg2,drop,first="I"):
+
+    print("--"*10)
+    mw = Markov_words()
+    mw.getdata(filename,drop)
+    mw.occurences(nlast=3)
+    mw.stats()
+    print("--"*10)
+    string = ""
+    ii = 0  
+    for i in mw.makeline(first,arg1,empiric=arg2,cutend=False):
+        ii += 1
+        string += " "+i 
+        if ii > 10 or i == ".":
+            string += "\n"
+            ii = 0
+    print(string) 
+    
+    
+i = input("give filename from homedir.\n")
+example(r1,100,10,drop) 
+    
