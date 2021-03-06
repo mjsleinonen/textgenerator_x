c0ea1e29a248aa477974d4ac935874dac3c5ff8d
mjsleinonen <37039101+mjsleinonen@users.noreply.github.com>
2021-03-07 00:03:04 +0200

    Update example.py

--- example.py
+++ example.py
@@ -7,14 +7,7 @@ def example(filename,arg1,arg2,drop,first="I"):
     mw.occurences(nlast=3)
     mw.stats()
     print("--"*10)
-    string = ""
-    ii = 0  
-    for i in mw.makeline(first,arg1,empiric=arg2,cutend=False):
-        ii += 1
-        string += " "+i 
-        if ii > 10 or i == ".":
-            string += "\n"
-            ii = 0
+    string = " ".join(mw.makeline(first,arg1,empiric=arg2,cutend=False)))
     print(string) 
     
     
